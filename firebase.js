a583adbfd782e686506c656f11876fb31ac73984
Nuku Malik Sanjaya Kusuma <nuku@softwareseni.com>
2021-09-04 12:29:56 +0700

    Add firebase and integrate realtime database

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAM0v4YhqxV60qMriyLpHAKgAnBdAGyHKU",
+  authDomain: "wedding-4f3cd.firebaseapp.com",
+  projectId: "wedding-4f3cd",
+  storageBucket: "wedding-4f3cd.appspot.com",
+  messagingSenderId: "993802808392",
+  appId: "1:993802808392:web:fd9ad7a12f8b20fb42bc7d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

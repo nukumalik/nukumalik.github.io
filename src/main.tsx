db4a7fe38099dcf66a4dc1679e59d18b0593fe03
Nuku Malik <nuku@softwareseni.com>
2024-10-01 09:25:04 +0700

    add redirection to custom domain

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './styles/global.scss'
 
+if (typeof window !== 'undefined') {
+  window.location.href = 'https://nukumalik.web.id'
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
